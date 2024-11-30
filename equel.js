3dd46bf4961f9da28677a4f013625c50cade6470
Shaheed mon <sheheedk07@gmail.com>
2024-11-30 23:23:36 +0530

    commit

--- equel.js
+++ equel.js
@@ -1,3 +1,6 @@
 const x=NaN
 const y=NaN
 console.log(x===y)
+
+//NaN not will equal to any data type
+// NaN is not a valid number, and thus cannot be compared to any other value, even to itself
---
+++ trueFalse.js
@@ -0,0 +1,6 @@
+let x=10>9>8;
+console.log(x) //false Q,why
+
+//explanation
+//10>9=true .value of true is 1
+//next contition not 9>8.contition is 1>8 .answer is false
