447b2680cb47db5c969cdfbfce3095878216b9e1
Shaheed mon <sheheedk07@gmail.com>
2024-12-01 09:57:39 +0530

    commit

---
+++ stringSum.js
@@ -0,0 +1,9 @@
+console.log("10"+"1")//101
+
+
+// 1.When "10" and "1" are enclosed in quotes,
+//  they are treated as strings, not numbers.
+
+// 2.Using the + operator between two strings ("10" and "1")
+//  causes them to be concatenated (joined together),
+//  resulting in the string "101".
