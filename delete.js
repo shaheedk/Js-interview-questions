62f237361811fd67966fc07d6fb49d0ce3e63ea8
Shaheed mon <sheheedk07@gmail.com>
2024-12-15 22:24:46 +0530

    initial commit

--- delete.js
+++ delete.js
@@ -2,3 +2,6 @@ const names='shaheed'
  age=18
 console.log(delete names)
 console.log(delete age )
+ 
+
+//why
---
+++ oprateLogic.js
@@ -0,0 +1,7 @@
+let x = 10 + 20 * 3;
+console.log(x);
+// 70
+// In JavaScript, the multiplication operator (*) has higher precedence than the addition operator (+).
+// Due to this reason, 20 * 3 is executed first, giving 60.
+// Then, the expression becomes 10 + 60.
+// Therefore, the output is 70.
