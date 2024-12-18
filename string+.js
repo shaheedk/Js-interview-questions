a929b6e075b16087e3627e0445bb03afdab26051
Shaheed mon <sheheedk07@gmail.com>
2024-12-18 15:51:33 +0530

    first commit

---
+++ string+.js
@@ -0,0 +1,4 @@
+console.log(2 + '4' + 5); // Output: '245'
+
+// In this case, the + operator performs concatenation because one of the operands ('4') is a string. 
+// JavaScript converts the other operands (2 and 5) to strings and concatenates them in order.
