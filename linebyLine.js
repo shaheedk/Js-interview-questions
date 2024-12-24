ed7f4184c844a0d4f0351e337b11aa3430117344
Shaheed mon <sheheedk07@gmail.com>
2024-12-24 10:30:09 +0530

    commit

--- linebyLine.js
+++ linebyLine.js
@@ -1,3 +1,7 @@
 const y=10
 const x=1+y;
 console.log(x)//11
+//output is 11 not error because 1+y is 1+10=11.
+// in this case 1 is not a string so it is not a string concatenation.
+//if 1 is a string then it is a string concatenation.
+//if 1 is a number then it is a number addition.
