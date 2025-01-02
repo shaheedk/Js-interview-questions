90a42d1c55c02a49fceafe6a07ada76c16409714
Shaheed mon <sheheedk07@gmail.com>
2025-01-02 09:11:38 +0530

    updated

---
+++ sameArr.js
@@ -0,0 +1,4 @@
+let arr1=[1,2]
+let arr2=[1,2]
+console.log(arr1==arr2)//false
+//output is false because arr1 and arr2 are two different arrays.
--- trueFalseVal.js
+++ trueFalseVal.js
@@ -1,4 +1,5 @@
-console.log(10>9>5)
+console.log(10>9>5)//falseconst arr1=[1,2]
+
 //output is false because 10>9 is true and true>5 is false.
 //true is converted to 1 and false is converted to 0.
 //the value of false is 0 and the value of true is 1.
