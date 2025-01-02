fd820b8a4200e22bc29465e4d3878b837d2dbd1e
Shaheed mon <sheheedk07@gmail.com>
2025-01-02 09:07:00 +0530

    commit

---
+++ trueFalseVal.js
@@ -0,0 +1,4 @@
+console.log(10>9>5)
+//output is false because 10>9 is true and true>5 is false.
+//true is converted to 1 and false is converted to 0.
+//the value of false is 0 and the value of true is 1.
