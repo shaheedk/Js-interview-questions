80d37a3ca7794004654a08da0e2f1981b94f34e2
Shaheed mon <sheheedk07@gmail.com>
2025-01-09 17:11:12 +0530

    commit

--- x++.js
+++ x++.js
@@ -4,3 +4,6 @@ console.log(x)//1
 
 
 //output is 1 because x++ is a post increment operator.
+//the value of x is first assigned to x and then incremented by 1.
+//so the value of x is 1 and then it is incremented by 1.
+//so the value of x is 1.
